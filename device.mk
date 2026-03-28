a44a0ade34be04cca950ba6e97e9af93719bde61
rio004 <riyon336@gmail.com>
2026-03-28 22:59:32 +0000

    Revert "duchamp: Build mediaTek WiFi lib and enable OUI support on wpa_supplicant_8"
    This reverts commit c4dc09e33fb4ec2655be4a49b377906c5259c663.
    
    Change-Id: I03671569b0c9db9aca880dc94003c9bc03655f90
    (cherry picked from commit fd825c5cb769ccf39518be89d3fcd6fc544ba974)

--- device.mk
+++ device.mk
@@ -327,7 +327,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \
-    hardware/mediatek/wlan/wifi_hal \
     hardware/xiaomi
 
 
@@ -361,6 +360,7 @@ $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
+    libwifi-hal-wrapper \
     wpa_supplicant
 
 # Inherit the proprietary files
