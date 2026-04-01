2b1a9abf3d12ceaaf7fd2405c8b5073d0d85a5c7
rio004 <riyon336@gmail.com>
2026-04-01 09:53:40 +0000

    duchamp: Drop per-app thermal parts
    Change-Id: I81ace3d0c7bd9ed3c6f9cbf71e39c3aa83ba9da4
    (cherry picked from commit 04c14af446794f25603d06088b37c793e7953d68)

--- parts/src/com/xiaomi/settings/BootCompletedReceiver.kt
+++ parts/src/com/xiaomi/settings/BootCompletedReceiver.kt
@@ -15,7 +15,6 @@ import android.util.Log
 import android.view.Display
 import android.view.Display.HdrCapabilities
 import com.xiaomi.settings.display.ColorService
-import com.xiaomi.settings.thermal.ThermalUtils
 import com.xiaomi.settings.turbocharging.TurboChargingService
 
 /** Everything begins at boot. */
@@ -40,9 +39,6 @@ class BootCompletedReceiver : BroadcastReceiver() {
     private fun onLockedBootCompleted(context: Context) {
         // Display
         context.startServiceAsUser(Intent(context, ColorService::class.java), UserHandle.CURRENT)
-
-        // Thermal
-        ThermalUtils.getInstance(context).startService()
 
         // Start TurboChargingService
         val turboChargingIntent = Intent(context, TurboChargingService::class.java)
