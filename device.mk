ba0a8f8d35615bab0ec163ebad1ca345b93e524e
Jose Miranda <josemiranda51263@gmail.com>
2026-04-14 02:44:55 +0000

    duchamp: fixing a small problem

--- device.mk
+++ device.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # A/B
 ifneq ($(WITH_GMS),true)
